44e06f0d50e4880461d575a5a3f6e61039de10ef
ericpastor <ericpastorpadial@gmai.com>
2023-04-06 08:52:03 +0300

    Exercise 13.1

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -1,16 +1,6 @@
 const express = require("express")
 const app = express()
 
-// let blogs = [
-//   {
-//     id: 1,
-//     author: "Edsger W. Dijkstra",
-//     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-//     title: "Go To Statement Considered Harmful",
-//     likes: 5,
-//   },
-// ]
-
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>")
 })
